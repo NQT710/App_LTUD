49482c426e043a2543a6d906be644ed7d0ea5193
ngocn <ngocnga25082001@gmail.com>
2023-06-09 16:35:13 +0700

    "Articles"

---
+++ Android/Vuive/app/src/main/java/com/nqt/vuive/model/Articles.kt
@@ -0,0 +1,12 @@
+package com.nqt.vuive.model
+
+class Articles(
+    var image_articles: String = "",
+    var title: String = "",
+    var avatar_author: String = "",
+    var name: String = "",
+    var day: String = "",
+    var tag_articles: String = "",
+    var description_articles: String = ""
+
+)
