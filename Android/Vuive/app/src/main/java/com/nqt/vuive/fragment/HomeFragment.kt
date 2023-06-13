9f71fa093222e65569017a4053b0465d725dffdc
Tran Ngoc Tai <tranngoctai28122001@gmail.com>
2023-06-14 02:17:38 +0700

    code have bug in  HomeFragment

--- Android/Vuive/app/src/main/java/com/nqt/vuive/fragment/HomeFragment.kt
+++ Android/Vuive/app/src/main/java/com/nqt/vuive/fragment/HomeFragment.kt
@@ -1,24 +1,42 @@
 package com.nqt.vuive.fragment
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import coil.load
 import com.bumptech.glide.Glide
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import com.nqt.vuive.R
 import com.nqt.vuive.databinding.FragmentHomeBinding
 import com.nqt.vuive.databinding.FragmentProfileBinding
 import com.nqt.vuive.viewmodel.AuthViewModel
 
 class HomeFragment : Fragment() {
-
-
+    private val GALLERY_REQUEST_CODE = 2
+    private val CAMERA_REQUEST_CODE = 1
     private lateinit var binding : FragmentHomeBinding
 
     private lateinit var viewModel: AuthViewModel
@@ -26,8 +44,14 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         binding = FragmentHomeBinding.inflate(inflater)
         viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+        binding.layoutIdentify.setOnClickListener {
+            cameraCheckPermission()
+        }
+
         return binding.root
     }
 
@@ -40,5 +64,118 @@ class HomeFragment : Fragment() {
             context?.let { Glide.with(it).load(data?.avatar).error(R.drawable.avatar_default).into(binding.imgAvatarHome) };
 
         })
+    }
+    private fun cameraCheckPermission() {
+
+        Dexter.withContext(requireActivity())
+            .withPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.CAMERA).withListener(
+
+                object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+
+                            if (report.areAllPermissionsGranted()) {
+                                camera()
+                            }
+
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?) {
+                        showRotationalDialogForPermission()
+                    }
+
+                }
+            ).onSameThread().check()
+    }
+    private fun camera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+    private fun showRotationalDialogForPermission() {
+        AlertDialog.Builder(requireActivity())
+            .setMessage("It looks like you have turned off permissions"
+                    + "required for this feature. It can be enable under App settings!!!")
+
+            .setPositiveButton("Go TO SETTINGS") { _, _ ->
+
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", requireContext().packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+
+            .setNegativeButton("CANCEL") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+    private fun galleryCheckPermission() {
+
+        Dexter.withContext(requireActivity()).withPermission(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                gallery()
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                Toast.makeText(
+                    requireContext(),
+                    "You have denied the storage permission to select image",
+                    Toast.LENGTH_SHORT
+                ).show()
+                showRotationalDialogForPermission()
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?, p1: PermissionToken?) {
+                showRotationalDialogForPermission()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun gallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        registerForActivityResult(intent, GALLERY_REQUEST_CODE)
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            when (requestCode) {
+
+                CAMERA_REQUEST_CODE -> {
+
+                    val bitmap = data?.extras?.get("data") as Bitmap
+
+                    //we are using coroutine image loader (coil)
+                    binding.imgAvatarHome.setImageBitmap(bitmap)
+                }
+
+                GALLERY_REQUEST_CODE -> {
+
+                    binding.imgAvatarHome.load(data?.data) {
+                        crossfade(true)
+                        crossfade(1000)
+                        // transformations(CircleCropTransformation())
+                    }
+
+                }
+            }
+
+        }
+
     }
 }
--- Android/Vuive/app/src/main/java/com/nqt/vuive/fragment/ProfileFragment.kt
+++ Android/Vuive/app/src/main/java/com/nqt/vuive/fragment/ProfileFragment.kt
@@ -8,33 +8,50 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nqt.vuive.R
 import com.nqt.vuive.activity.LoginActivity
 import com.nqt.vuive.databinding.FragmentHomeBinding
 import com.nqt.vuive.databinding.FragmentOnboardBinding
 import com.nqt.vuive.databinding.FragmentProfileBinding
 import com.nqt.vuive.viewmodel.AuthViewModel
+import kotlinx.android.synthetic.main.fragment_profile.pagerTest
+import kotlinx.android.synthetic.main.fragment_profile.tabDemo
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 class ProfileFragment  : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
+    private lateinit var pagerTest: ViewPager2
+    private lateinit var tabDemo: TabLayout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        pagerTest = view.findViewById(R.id.pagerTest)
+        tabDemo = view.findViewById(R.id.tabDemo)
+        val adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+        pagerTest.adapter = adapter
+        TabLayoutMediator(tabDemo,pagerTest){tab,position ->
+            when(position){
+                0->{
+                    tab.text="ARTICLES"
+                }
+                1->{
+                    tab.text="SPECIES"
+                }
+
+
+            }
+        }.attach()
+
+        return view
+
     }
 
 
